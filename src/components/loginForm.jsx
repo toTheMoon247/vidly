f1e437763f4af89c0c3b5cda1d6ed22f0d4c42ea
Sam H <samuel1haim@gmail.com>
2021-01-05 11:01:07 +0000

    Completed login form

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Joi from 'joi-browser';
 import Input from './common/input';
 
 class LoginForm extends Component {
@@ -8,15 +9,23 @@ class LoginForm extends Component {
     errors: {}
   }
 
-  validate = () => {
-    const errors = {};
-    const { account } = this.state;
-    if (account.username.trim() === '')
-      errors.username = 'Username is required.';
-    if (account.password.trim() === '')
-      errors.password = 'Password is required.';
+  schema = {
+    username: Joi.string().required().label('Username'),
+    password: Joi.string().required().label('Password')
+  };
 
-    return Object.keys(errors).length === 0 ? null : errors;
+  validate = () => {
+    const options = {abortEarly: false};
+    // Using Joi library to validate the inputs
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    if (!error)
+      return null;
+
+    const errors = {};
+    for (let item of error.details)
+      errors[item.path[0]] = item.message;
+
+    return errors;
   }
 
   handleSubmit = e => {
@@ -33,14 +42,11 @@ class LoginForm extends Component {
   }
 
   validateProperty = ({ name, value }) => {
-    if (name === 'username') {
-      if (value.trim() === '')
-        return 'Username is required.';
-    }
-    if (name === 'password') {
-      if (value.trim() === '')
-        return 'Password is required.';
-    }
+    // [name] is a computed property ---> TO DO: Read more
+    const obj = { [name]: value};
+    const schema = {[name]: this.schema[name]};
+    const {error} = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
   }
 
   handleChange = ({ currentTarget: input }) => {
@@ -78,7 +84,9 @@ class LoginForm extends Component {
             onChange={this.handleChange}
             error={errors.password}
           />
-          <button className="btn btn-primary">Login</button>
+          <button
+            disabled={this.validate()}
+            className="btn btn-primary">Login</button>
         </form>
 
       </div>
