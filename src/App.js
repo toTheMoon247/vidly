37a11c2d7d4b9b71456a2b6c76dc90436e5a2fb5
Sam H <samuel1haim@gmail.com>
2021-01-05 09:55:20 +0000

    added basic navbar. Added login form with basic validation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import React, { Component } from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import Movies from './components/movies';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
+import NavBar from './components/navBar';
+import MovieForm from './components/movieForm';
+import LoginForm from './components/loginForm';
 import './App.css';
 
 function App() {
   return (
-    <main className="container">
-      <h1>Hello World!</h1>
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/login" component={LoginForm}/>
+          <Route path="/movies/:id" component={MovieForm}/>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
-import Movies from './components/movies';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Movies />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
